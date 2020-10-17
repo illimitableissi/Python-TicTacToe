3c5b7ef99b3b99e91ac26360e4df8c3bda453be1
SOVEREIGN <isimemeni@gmail.com>
2020-10-17 17:07:18 -0400

    adding functions to game

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,2 +1,24 @@
 # Python Tic Tac Toe
 
+game_row1 = ['','','']
+game_row2 = ['','','']
+game_row3 = ['','','']
+
+def display(row1,row2,row3):
+    """
+    Display function to show game board
+    """
+    pass
+    print(row1)
+    print(row2)
+    print(row3)
+
+display(game_row1, game_row2, game_row3)
+
+def user_pick(pick):
+    """
+    Updates gameboard with user pick
+    """
+    pass
+    pick = input("Please enter a value: ")
+
